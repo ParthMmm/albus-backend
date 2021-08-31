0391e48c89e07e7c992b106eb0678f71e755c890
parthmangrola <parthmangrola@gmail.com>
2021-08-31 14:01:09 -0700

    :construction: Working on action handling

--- routes/mbRoutes.js
+++ routes/mbRoutes.js
@@ -16,26 +16,12 @@ router.get("/album_id", async (req, res, next) => {
   const result = await mbApi.search("release", {
     artist: artist,
     release: album,
-    country: "US",
-    primarytype: "Album",
+    // country: "US",
+    // primarytype: "Album",
   });
   console.log(result);
   res.json(result.releases[0]);
   //   res.json(result.releases[0]);
 });
-async function hello() {
-  const result = await mbApi.search("release", {
-    artist: "madonna",
-    release: "confessions on a dance floor",
-    country: "US",
-    primarytype: "Album",
-  });
-  console.log(result.releases[0]);
-
-  const recording = await mbApi.getRecording(
-    "0ace9b23-934d-4b58-a749-f0f33b883646"
-  );
-  console.log(recording);
-}
 
 module.exports = router;
--- routes/secureRoutes.js
+++ routes/secureRoutes.js
@@ -6,21 +6,49 @@ const UserModel = require("../models/User");
 const { ConnectionStates } = require("mongoose");
 const router = require("express").Router();
 
-router.post("/addListened", async (req, res, next) => {
-  const body = Object.keys(req.body).toString();
-  console.log(body);
-  console.log(req.user._id);
-  const id = req.user._id;
+const checkAction = async (userID, mbID) => {
+  let listened = false;
+  let wantToListen = false;
+  let listening = false;
 
-  UserModel.findByIdAndUpdate(
-    id,
-    {
-      $push: { listened: { mbid: body } },
-    },
-    { new: true }
-  ).then((result) => {
-    res.status(200).send(result);
+  const checkListened = await UserModel.findById(userID, {
+    listened: { $elemMatch: { mbid: mbID } },
   });
+  const checkWantToListen = await UserModel.findById(userID, {
+    wantToListen: { $elemMatch: { mbid: mbID } },
+  });
+  const checkListening = await UserModel.findById(userID, {
+    listening: { $elemMatch: { mbid: mbID } },
+  });
+
+  console.log(checkListened);
+  console.log(checkWantToListen);
+  console.log(checkListening);
+  console.log(checkListened.listened);
+  if (checkListened != null) {
+    if (checkListened.listened != "") {
+      listened = true;
+    }
+  }
+  console.log(listened);
+  return listened;
+};
+
+router.post("/addListened", async (req, res, next) => {
+  const mbID = Object.keys(req.body).toString();
+  const userID = req.user._id;
+  let tmp = await checkAction(userID, mbID);
+  if (!tmp) {
+    UserModel.findByIdAndUpdate(
+      userID,
+      {
+        $push: { listened: { mbid: mbID } },
+      },
+      { new: true }
+    ).then((result) => {
+      res.status(200).send(result);
+    });
+  }
 });
 
 module.exports = router;
